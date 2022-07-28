f2792e2c03c37f6a89e585c075f8a52a86b302be
Richard Aiello <richaiello17@gmail.com>
2022-07-28 13:14:05 -0400

    Styles and JSX for SharedHero.jsx on mobile.

---
+++ src/components/shared/SharedHero.jsx
@@ -0,0 +1,38 @@
+import '../../styles/shared.css';
+
+const SharedHero = props => {
+  return (
+    <section className="container--small relative h-[240px] w-full mb-[365px]">
+      <div className="absolute bg-white max-w-[343px] h-max left-0 top-[195px] pl-8 pr-4 pt-16 sm:pr-0">
+        <h1 className="text-black-custom font-bold text-[3rem] leading-[3.25rem] -tracking-[1.2px] mb-5">
+          {props.heading}
+        </h1>
+        <p className="text-grey-dark-custom">{props.text}</p>
+      </div>
+      <picture>
+        <source
+          media="(min-width: 1440px)"
+          srcSet={`./${props.mod}/desktop/image-hero.jpg`}
+        />
+        <source
+          media="(min-width: 500px)"
+          srcSet={`./${props.mod}/tablet/image-hero.jpg`}
+        />
+        <img
+          src={`./${props.mod}/mobile/image-hero.jpg`}
+          alt=""
+          className="relative h-full w-full -z-10 object-cover"
+        />
+      </picture>
+      <span
+        aria-hidden
+        className="hidden md:block absolute right-0 top-[204px] text-silver-custom
+        font-bold text-[7.5rem] leading-[12.5rem] -tracking-[3px]"
+      >
+        {props.flavor}
+      </span>
+    </section>
+  );
+};
+
+export default SharedHero;
--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,5 +1,18 @@
+import SharedHero from '../components/shared/SharedHero';
+
 const About = props => {
-  return null;
+  return (
+    <main className="mt-24">
+      <SharedHero
+        heading="Your team of professionals"
+        text="Our small team of world-class professionals will work with you every step of the way. 
+          Strong relationships are at the core of everything we do. This extends to the 
+          relationship our projects have with their surroundings."
+        flavor="About"
+        mod="about"
+      />
+    </main>
+  );
 };
 
 export default About;
