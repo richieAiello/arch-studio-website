fed1385adf4eaa0a054b7a43042d936f6a6baebe
Richard Aiello <richaiello17@gmail.com>
2022-07-28 14:10:51 -0400

    Implemented a scroll to top feature on route change.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 import Header from './components/header/Header';
 import Home from './routes/Home';
 import Portfolio from './routes/Portfolio';
@@ -9,14 +10,16 @@ import Footer from './components/footer/Footer';
 const App = props => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+      <ScrollToTop>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </ScrollToTop>
     </BrowserRouter>
   );
 };
