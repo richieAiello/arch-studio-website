304a5066c362017fe1961edb97ae1a2b5b846b5a
Richard Aiello <richaiello17@gmail.com>
2022-07-30 10:23:46 -0400

    Styles for Home.jsx on tablet.

--- src/components/home/featured/Featured.jsx
+++ src/components/home/featured/Featured.jsx
@@ -3,22 +3,27 @@ import ArrowLink from '../../shared/ArrowLink';
 
 const Featured = props => {
   return (
-    <section className="container mb-[8.25rem]">
-      <h2 className="heading text-black-custom mb-[2.625rem]">
+    <section className="container mb-[8.25rem] md:grid md:grid-rows-[auto,auto] md:grid-cols-2 md:mb-[12.5rem]">
+      <h2 className="heading text-black-custom mb-[2.625rem] md:mb-[5.375rem]">
         Featured
       </h2>
-      <FeaturedCard
-        heading="Project Del Sol"
-        text="1"
-        image="del-sol"
-      />
-      <FeaturedCard heading="228B Tower" text="2" image="228b" />
-      <FeaturedCard
-        heading="Le Prototype"
-        text="3"
-        image="prototype"
-      />
-      <ArrowLink path="portfolio" className="justify-center">
+      <div className=" row-start-2 col-span-2">
+        <FeaturedCard
+          heading="Project Del Sol"
+          text="1"
+          image="del-sol"
+        />
+        <FeaturedCard heading="228B Tower" text="2" image="228b" />
+        <FeaturedCard
+          heading="Le Prototype"
+          text="3"
+          image="prototype"
+        />
+      </div>
+      <ArrowLink
+        path="portfolio"
+        className="justify-center md:self-start md:justify-self-end md:pr-8 md:pl-[2.1875rem]"
+      >
         Portfolio
       </ArrowLink>
     </section>
