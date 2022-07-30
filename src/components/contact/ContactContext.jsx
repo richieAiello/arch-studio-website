2ad30ec060d98153f561bae37ad32b84e9a972d8
Richard Aiello <richaiello17@gmail.com>
2022-07-30 15:06:31 -0400

    Styles for AboutContext.jsx and ContactContext.jsx on tablet.
    Adjusted the accent classes in index.css and distributed them accordingly.

--- src/components/contact/ContactContext.jsx
+++ src/components/contact/ContactContext.jsx
@@ -1,6 +1,6 @@
 const ContactContext = props => {
   return (
-    <section className="container accent mb-[4.5rem]">
+    <section className="container accent--secondary mb-[4.5rem]">
       <h2 className="heading text-black-custom capitalize max-w-[8ch]">
         Contact Details
       </h2>
--- src/components/shared/SharedHero.jsx
+++ src/components/shared/SharedHero.jsx
@@ -7,7 +7,7 @@ const SharedHero = props => {
       md:h-[720px] md:mb-[12.5rem]"
     >
       <div
-        className="accent--secondary absolute bg-white max-w-[343px] h-max left-0 top-[195px] pl-8 pr-4 pt-16 
+        className="accent--hero absolute bg-white max-w-[343px] h-max left-0 top-[195px] pl-8 pr-4 pt-16 
         sm:pr-0 md:right-0 md:bottom-0 md:left-[unset] md:top-[unset] md:max-w-[515px] md:pt-[5.5rem] md:pl-[3.6875rem]"
       >
         <h1 className="heading text-black-custom mb-5 md:mb-[2.375rem]">
@@ -31,7 +31,7 @@ const SharedHero = props => {
         />
       </picture>
       <FlavorText
-        className="top-[302px] right-1"
+        className="top-[300px] right-1"
         text={props.flavor}
       />
     </section>
