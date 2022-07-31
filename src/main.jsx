90c9e4fbf198cfb6bf2976c7fe4570da58da020e
Richard Aiello <richaiello17@gmail.com>
2022-07-31 15:11:09 -0400

    Linked leaflet styles to index.html.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import '../node_modules/leaflet/dist/leaflet.css';
 import './styles/nav.css';
 import './styles/footer.css';
 
