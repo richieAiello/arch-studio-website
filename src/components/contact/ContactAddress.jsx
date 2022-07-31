b60848b3ffb4a3cb4cef722a16e4dd9f15a35310
Richard Aiello <richaiello17@gmail.com>
2022-07-31 12:40:35 -0400

    Styles for ContactContext.jsx on desktop.

--- src/components/contact/ContactAddress.jsx
+++ src/components/contact/ContactAddress.jsx
@@ -2,11 +2,14 @@ import { ReactComponent as Arrow } from '../../assets/arrow-dark.svg';
 
 const ContactAddress = props => {
   return (
-    <div className="text-grey-dark-custom mt-10 md:mt-[2.625rem] md:grid md:grid-cols-[1fr,1fr]">
+    <div
+      className="text-grey-dark-custom mt-10 md:mt-[2.625rem] md:grid md:grid-cols-[1fr,1fr] 
+      lg:block lg:mt-0"
+    >
       <h3 className="capitalize font-bold leading-[2.1875rem] mb-[0.875rem]">
         {props.heading}
       </h3>
-      <address className="mb-[2.6875rem] not-italic md:col-start-1">
+      <address className="mb-[2.6875rem] not-italic md:col-start-1 lg:mb-[3.6875rem]">
         <p className="">Mail : {props.mail}</p>
         <p className="">Address: {props.address}</p>
         <p className="">Phone: {props.phone}</p>
