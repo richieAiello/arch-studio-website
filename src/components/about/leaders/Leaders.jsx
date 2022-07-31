1d8b1479a218ac10f6497f81ac12d714cda07e28
Richard Aiello <richaiello17@gmail.com>
2022-07-31 12:28:46 -0400

    Styles for Leaders.jsx on desktop.

--- src/components/about/leaders/Leaders.jsx
+++ src/components/about/leaders/Leaders.jsx
@@ -2,7 +2,10 @@ import LeadersGrid from './LeadersGrid';
 
 const Leaders = props => {
   return (
-    <section className="container--leaders mb-[10.6875rem] md:mb-[14.9375rem]">
+    <section
+      className="container--leaders mb-[10.6875rem] md:mb-[14.9375rem] 
+      lg:grid lg:grid-cols-[auto,1fr] lg:mb-40"
+    >
       <h2 className="heading text-black-custom capitalize max-w-[8ch] mb-[3.4375rem] md:mb-[3.0625rem]">
         The Leaders
       </h2>
